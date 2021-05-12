5187960e9a2c7ee01e8140c14d1f5523cf2b5f31
witby <47345574+witby@users.noreply.github.com>
2021-05-12 21:56:32 +0800

    Update commands.py

--- commands/commands.py
+++ commands/commands.py
@@ -22,7 +22,7 @@ keyboard = [
  InlineKeyboardButton("Source Code", url=Config.SOURCE)
 ],
 [
- InlineKeyboardButton("How To Create A Bot Like Me",url="https://youtu.be/swg6un2N4Fk")
+ InlineKeyboardButton("How To Create A Bot Like Me",url="https://github.com/witby/ForwardTagRemoverBot")
 ]
 ]
 
